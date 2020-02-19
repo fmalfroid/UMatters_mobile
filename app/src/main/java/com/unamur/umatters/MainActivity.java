39b3c230e6de6e3ec12644f5bb521268c97e0c5f
Mimie8 <amelie.dieudonne@student.unamur.be>
2020-02-19 19:48:21 +0100

    Resolve conflict

--- app/src/main/java/com/unamur/umatters/MainActivity.java
+++ app/src/main/java/com/unamur/umatters/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //SALUT
+    //coucou
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
