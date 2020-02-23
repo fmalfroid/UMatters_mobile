64c31f1ed512560c4050c8d21c000e58a2223e61
Mimie8 <amelie.dieudonne@student.unamur.be>
2020-02-23 11:44:10 +0100

    Change switch on color

--- app/src/main/java/com/unamur/umatters/MainActivity.java
+++ app/src/main/java/com/unamur/umatters/MainActivity.java
@@ -13,8 +13,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
